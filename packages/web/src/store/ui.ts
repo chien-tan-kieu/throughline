43bc071ae02765cc51958f7cd0e272daf8ef4cb3
chien.tankieu <chien.tankieu@gmail.com>
2026-05-21 00:54:09 +0700

    feat(web): add useUiStore for story filter state

---
+++ packages/web/src/store/ui.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+type StoryFilter = "all" | "backlog" | "in-progress" | "done";
+
+interface UiState {
+  storyFilter: StoryFilter;
+  setStoryFilter: (f: StoryFilter) => void;
+}
+
+export const useUiStore = create<UiState>((set) => ({
+  storyFilter: "all",
+  setStoryFilter: (storyFilter) => set({ storyFilter }),
+}));
