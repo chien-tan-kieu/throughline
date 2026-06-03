b435dd46dfebd8edb14cb0700b9a2f108e17c193
chien.tankieu <chien.tankieu@gmail.com>
2026-06-04 03:52:52 +0700

    feat(web): route /docs to DocsPage, redirect / /plan /spec

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,8 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Sidebar } from "./components/layout/Sidebar.tsx";
 import { Topbar } from "./components/layout/Topbar.tsx";
 import { useWebSocket } from "./hooks/useWebSocket.ts";
-import { PlanPage } from "./pages/PlanPage.tsx";
-import { SpecPage } from "./pages/SpecPage.tsx";
+import { DocsPage } from "./pages/DocsPage.tsx";
 import { StandupPage } from "./pages/StandupPage.tsx";
 import { StoriesPage } from "./pages/StoriesPage.tsx";
 import { StoryPage } from "./pages/StoryPage.tsx";
@@ -17,11 +16,12 @@ function Shell() {
         <Sidebar />
         <main className="main">
           <Routes>
-            <Route path="/" element={<PlanPage />} />
-            <Route path="/plan" element={<PlanPage />} />
+            <Route path="/" element={<Navigate to="/docs" replace />} />
+            <Route path="/plan" element={<Navigate to="/docs" replace />} />
+            <Route path="/spec" element={<Navigate to="/docs?tab=spec" replace />} />
+            <Route path="/docs" element={<DocsPage />} />
             <Route path="/stories" element={<StoriesPage />} />
             <Route path="/story/:id" element={<StoryPage />} />
-            <Route path="/spec" element={<SpecPage />} />
             <Route path="/standup" element={<StandupPage />} />
           </Routes>
         </main>
